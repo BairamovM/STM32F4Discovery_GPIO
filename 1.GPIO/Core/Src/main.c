02023af6c1792675b7b16503d12e7efb243f46dd
BairamovM <muslimbairamov@gmail.com>
2018-07-10 11:40:51 +0300

    first fearute: in init blink all LEDs for 1s

--- 1.GPIO/Core/Src/main.c
+++ 1.GPIO/Core/Src/main.c
@@ -102,16 +102,10 @@ int main(void)
 
 //---- Test LEDS --------------------------------------------------------------------------------
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET); // Turn on LED3_Orange
-  HAL_Delay(500);                                          // Delay 500 ms
-
   HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET); // Turn on LED4_Green
-  HAL_Delay(500);                                          // Delay 500 ms
-
   HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET); // Turn on LED5_Red
-  HAL_Delay(500);                                          // Delay 500 ms
-
   HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET); // Turn on LED6_Blue
-  HAL_Delay(500);                                          // Delay 500 ms
+  HAL_Delay(1000);                                          // Delay 500 ms
 
 
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET); // Turn off LED3_Orange
